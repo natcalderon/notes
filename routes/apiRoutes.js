66f02de80858e4c5975334137830e1cb68775a1f
natcalderon <nat.calderon04@gmail.com>
2021-06-01 10:56:36 -0600

    jquery

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,8 @@
 const fs = require("fs");
-const notesData = require("../db/db.json");
+const data = require("../db/db.json");
 
 module.exports = function(app){
-
-
-   
     function writeToDB(notes){
-        
         notes = JSON.stringify(notes);
         console.log (notes);
         
@@ -17,31 +13,25 @@ module.exports = function(app){
         });
     }
 
-    //========== API ROUTES ==========
-
-  
-    app.get("/api/notes", function(req, res){
-        res.json(notesData);
+     app.get("/api/notes", function(req, res){
+        res.json(data);
     });
 
     
     app.post("/api/notes", function(req, res){
 
-   
-        if (notesData.length == 0){
+        if (data.length == 0){
             req.body.id = "0";
         } else{
-            req.body.id = JSON.stringify(JSON.parse(notesData[notesData.length - 1].id) + 1);
+            req.body.id = JSON.stringify(JSON.parse(data[data.length - 1].id) + 1);
         }
         
         console.log("req.body.id: " + req.body.id);
+        data.push(req.body);
 
      
-        notesData.push(req.body);
-
-     
-        writeToDB(notesData);
-        console.log(notesData);
+        writeToDB(data);
+        console.log(data);
 
       
         res.json(req.body);
@@ -55,21 +45,14 @@ module.exports = function(app){
         console.log(id);
 
         
-        for (i=0; i < notesData.length; i++){
-           
-            if (notesData[i].id == id){
+        for (i=0; i < data.length; i++){
+           if (data[i].id == id){
                 console.log("delete");
-              
                 res.send(notesData[i]);
-
-              
                 notesData.splice(i,1);
                 break;
             }
         }
-
-        
-        writeToDB(notesData);
-
-    });
+         writeToDB(notesData);
+ });
 };
